9e8556178c497d41749d1c4b2288b7adc804f17b
csaba-csete-87 <csaba.csete@gmail.com>
2019-10-23 22:34:46 +0300

    Worked on pb 5

--- BasicAlgorithms/problem_5.py
+++ BasicAlgorithms/problem_5.py
@@ -1,37 +1,45 @@
 ## Represents a single node in the Trie
 class TrieNode:
     def __init__(self):
-        ## Initialize this node in the Trie
-        pass
+        self.is_word = False
+        self.children = {}
 
     def insert(self, char):
-        ## Add a child node in this Trie
-        pass
+        if char not in self.children:
+            node = TrieNode()
+            self.children[char] = node
 
-    def suffixes(self, suffix = ''):
+    def suffixes(self, suffix=''):
         ## Recursive function that collects the suffix for
         ## all complete words below this point
         pass
 
+
 ## The Trie itself containing the root node and insert/find functions
 class Trie:
     def __init__(self):
-        ## Initialize this Trie (add a root node)
-        pass
+        self.root = TrieNode()
 
     def insert(self, word):
-        ## Add a word to the Trie
-        pass
+        current_node = self.root
+        for i in range(0, len(word)):
+            char = word[i]
+            current_node.insert(char)
+            current_node = current_node.children[char]
+        current_node.is_word = True
 
     def find(self, prefix):
         ## Find the Trie node that represents this prefix
         pass
 
-MyTrie = Trie()
+
+my_trie = Trie()
 wordList = [
     "ant", "anthology", "antagonist", "antonym",
     "fun", "function", "factory",
     "trie", "trigger", "trigonometry", "tripod"
 ]
 for word in wordList:
-    MyTrie.insert(word)
+    my_trie.insert(word)
+
+print(my_trie.find("f"))
