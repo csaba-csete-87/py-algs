b976036ba5d9c1e44175ac27794d5f55cb9a918b
Csaba Csete <csaba.csete@gmail.com>
2019-10-23 14:00:13 +0300

    Solved problem 4, in place, single traversal

--- BasicAlgorithms/problem_4.py
+++ BasicAlgorithms/problem_4.py
@@ -1,12 +1,26 @@
 def sort_012(input_list):
+    i_0 = 0
+    i_2 = len(input_list) - 1
     i = 0
-    for n in range(0, len(input_list) - 1):
+    while i <= i_2:
         crt = input_list[i]
         if crt < 1:
-            input_list.insert(0, input_list.pop(i))
+            if input_list[i_0] != 0:
+                start = input_list[i_0]
+                input_list[i_0] = crt
+                input_list[i] = start
             i += 1
+            i_0 += 1
         elif crt > 1:
-            input_list.append(input_list.pop(i))
+            if input_list[i_2] == 2:
+                i_2 -= 1
+            else:
+                end = input_list[i_2]
+                input_list[i_2] = crt
+                input_list[i] = end
+                i_2 -= 1
+                if end == 1:
+                    i += 1
         else:
             i += 1
     return input_list
@@ -19,6 +33,9 @@ def test_function(test_case):
     else:
         print("Fail")
 
+test_function([1, 1, 1, 0, 0, 0, 2, 2, 2])
 test_function([0, 0, 2, 2, 2, 1, 1, 1, 2, 0, 2])
 test_function([2, 1, 2, 0, 0, 2, 1, 0, 1, 0, 0, 2, 2, 2, 1, 2, 0, 0, 0, 2, 1, 0, 2, 0, 0, 1])
 test_function([0, 0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 1, 2, 2, 2, 2, 2, 2, 2])
+test_function([0,0,2,0,0,0,1,0,2])
+test_function([0,0,1,0,1,0,0,0,1])
